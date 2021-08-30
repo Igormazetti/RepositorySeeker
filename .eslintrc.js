d23005f4db18448a1a025402ea5a56fae2450069
IgorMazetti <iigormazetti@hotmail.com>
2021-08-30 20:03:34 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'no-unused-vars': 'off',
+    'react/prop-types': 'off',
+    'react/static-property-placement': 'off',
+    'no-console': 'off',
+    'react/state-in-constructor': ['off'],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+  },
+};
